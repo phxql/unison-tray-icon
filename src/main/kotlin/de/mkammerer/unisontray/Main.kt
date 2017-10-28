60ab82c38e4f2fd6bed1d6ec94ed3165651d3693
Moritz Kammerer <kammerer.moritz@gmail.com>
2017-10-28 10:10:52 +0200

    Add SLF4J and Logback
    Configure logging.

--- src/main/kotlin/de/mkammerer/unisontray/Main.kt
+++ src/main/kotlin/de/mkammerer/unisontray/Main.kt
@@ -1,5 +1,22 @@
 package de.mkammerer.unisontray
 
+import org.slf4j.LoggerFactory
+
 fun main(args: Array<String>) {
-    println("Hello world")
+    Main.start(args)
+}
+
+object Main {
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun start(args: Array<String>) {
+        logger.info("Started")
+        try {
+            println("Hello world")
+        } catch (e: Exception) {
+            logger.error("Unhandled exception occurred", e)
+        } finally {
+            logger.info("Stopped")
+        }
+    }
 }
